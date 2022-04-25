cdefad796af22ce8b151ddd0280465c18687002a
king-yahassan <king.yahassane@gmail.com>
2022-04-25 15:16:52 +0000

    Projets Web1

---
+++ mon projet 2/mon projet 2/Projet.js
@@ -0,0 +1,17 @@
+function valider() {
+    
+    var Identifiant = document.getElementById("identifiant").value;
+    var Motdepasse = document.getElementById("motdepasse").value;
+
+   //if (Identifiant.length=="admin" && Motdepasse=="user"){
+   //  alert("Connexion réussie!");
+   //  return false;
+   // }
+   
+     //else
+       //{
+         //  alert("Echec de la connexion!")
+      // }
+   //}
+
+  
